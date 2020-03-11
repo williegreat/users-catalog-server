d4eb760d3868c981bd3ec559cd4aaddba1bb48f3
Eli Shalman <williegreat@gmail.com>
2020-03-11 22:37:20 +0200

    Update Member.js

--- models/Member.js
+++ models/Member.js
@@ -3,11 +3,28 @@ var mongoose = require("mongoose");
 var MemberSchema = new mongoose.Schema(
     {
         id: { type: Number, required: [true, "can't be blank"] },
-        firstName: { type: String, required: [true, "can't be blank"] },
+        firstName: {
+            type: String,
+            max: 20,
+            required: [true, "can't be blank"]
+        },
         lastName: { type: String, required: [true, "can't be blank"] },
-        gender: { type: String, required: [true, "can't be blank"] },
-        maritalStatus: { type: String, required: [true, "can't be blank"] },
-        age: { type: String, required: [true, "can't be blank"] },
+        gender: {
+            type: String,
+            enum: ["f", "m"],
+            required: [true, "can't be blank"]
+        },
+        maritalStatus: {
+            type: String,
+            enum: ["s", "m", "d", "w"],
+            required: [true, "can't be blank"]
+        },
+        age: {
+            type: Number,
+            min: 0,
+            max: 120,
+            required: [true, "can't be blank"]
+        },
         kids: [{ name: "string" }]
     },
     { timestamps: true }
